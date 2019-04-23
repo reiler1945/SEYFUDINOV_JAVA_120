825d1ff673bd4f7f708dd563c2c647acfdf89c9c
Наиль Сейфудинов <reiler1945@gmail.com>
2019-04-23 19:53:25 +0300

    add ClassWork_1 started

---
+++ Classworks/classworks_1/Program.java
@@ -0,0 +1,15 @@
+class Program {
+	public static void main(String args[]) {
+		int number = 135;
+		System.out.println(
+			          		char(int(number / (2^7)) + _
+							char(int(number / (2^6)) + _
+							char(int(number / (2^5)) + _
+							char(int(number / (2^4)) + _
+							char(int(number / (2^3)) + _
+							char(int(number / (2^2)) + _
+							char(int(number / (2^1)) + _
+							char(int(number / (2^0)) + _	
+						  );
+	}
+}
