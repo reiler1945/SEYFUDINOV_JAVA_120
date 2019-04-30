8a3f6bbfaba949c011e57293b769def95bddc76a
Наиль Сейфудинов <reiler1945@gmail.com>
2019-04-30 20:11:22 +0300

    сортировка выбором

--- Classworks/Classwork_2/Program.java
+++ Classworks/Classwork_2/Program.java
@@ -4,14 +4,16 @@ class Program{
 		int a[] = {-1, 5, -3, 20, 11, 0, 2, 6};
 		for (int k = 0; k < a.length; k++) {
 			int min = a[k];
-			int i_min = k;
+			int index_min = k;
 			for (int i = k; i < a.length; i++) {
-				i_min = min > a[i] ? i : i_min;
-				min = min > a[i] ? a[i] : min; 
+				if (min > a[i]) {
+					index_min = i;
+					min = a[i];
+				}
 			}	
 			int temp = a[k];	
-			a[k] = a[i_min];
-			a[i_min] = temp;
+			a[k] = a[index_min];
+			a[index_min] = temp;
 		}
 		for (int i = 0; i < a.length; i++) {
 			System.out.println(a[i]);
