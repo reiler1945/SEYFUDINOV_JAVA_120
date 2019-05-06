2bc543438802f3787fff43e42b47256b2c207904
Наиль Сейфудинов <reiler1945@gmail.com>
2019-05-06 19:07:13 +0300

    fixed

--- Classworks/SingleListReverse/Program.java
+++ Classworks/SingleListReverse/Program.java
@@ -1,41 +1,50 @@
+/*
+пример односвязного списка
+*/
+import java.util.Scanner;
 class Program {
-	public static void main(String[] args) {
-		// initialization списка
-		SList list = new SList();
-		SList temp = list;
-		for (int i = 0; i < 10; i++) {
-			temp.value = i * 5;
-			temp.next = new SList();
-			temp = temp.next;	
-		};
-		// print list
-		System.out.println("Straight:");
-		temp = list;
-		while (temp.next != null) {
-			System.out.println("value = " + temp.value);
-			temp = temp.next;	
-		};
-		// do reverse
-		SList previous = null;
-		SList current = list;
-		SList forward; 
-		while (current != null) {
-			forward = current.next;
-			current.next = previous;
-			previous = current;
-			current = forward;
-		}
-		// print reverse list
-		System.out.println("Reverse:");
-		temp = previous;
-		while (temp.next != null) {
-			System.out.println("value = " + temp.value);
-			temp = temp.next;	
-		}
-	}
+    public static void main(String args[]){
+       SList list = new SList();
+       SList temp = list;
+       temp.value = 0;
+       // инициализация листа
+       for (int i = 1; i < 10; i++) {
+                       SList buf = temp;
+                       temp.next = new SList();
+                       temp = temp.next;
+                       temp.value = buf.value + 5;
+       }
+       // отобразим значения листа
+       temp = list;
+       while (temp != null) {
+                       System.out.println(temp.value);
+                       temp = temp.next;
+       }
+       System.out.println("------------------------------------");
+       // реверсируем лист
+       SList previous = null;
+       SList current = list;
+       SList forward = list.next;
+       while (forward != null) {
+                       current.next = previous;
+                       previous = current;
+                       current = forward;
+                       forward = current.next;
+       }
+       current.next = previous;            
+       System.out.println("------------------------------------");                  
+       // отобразим reversed list
+       temp = current;
+       int i = 0;
+       while (temp != null) {
+                       System.out.println(temp.value);
+                       temp = temp.next;
+       }
+       System.out.println("------------------------------------");                  
+    }
 }
-
+ 
 class SList {
-	int value;
-	SList next;
+                int value;
+                SList next;
 }
