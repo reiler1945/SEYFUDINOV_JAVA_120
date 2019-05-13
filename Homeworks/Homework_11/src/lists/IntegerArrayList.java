a79779abde34fe7c9ab660c607184dc1559eb232
reiler <reiler@ya.ru>
2019-05-13 21:12:21 +0300

    Задание 11 часть list massive

--- Homeworks/Homework_11/src/lists/IntegerArrayList.java
+++ Homeworks/Homework_11/src/lists/IntegerArrayList.java
@@ -3,17 +3,20 @@ package lists;
 public class IntegerArrayList {
 
     int[] array;
-    int currentLength = 0;
+    int currentLength = 0; //актуальная длина списка
 
+    //добавим элемент в список
     void add(int element) {
         if (currentLength == 10) {
-            System.out.println("Can't add element. The list is full");
+            System.out.println("Can't add element " + element + ". The list is full");
         } else {
             array[currentLength] = element;
             currentLength++;
+            System.out.println("added element = " + element + " by index = " + (currentLength - 1));
         }
     }
 
+    //элемент по индексу
     int get(int index) {
         if ((index < currentLength) && (index >= 0)) {
             return array[index];
@@ -21,16 +24,35 @@ public class IntegerArrayList {
         return -1;
     }
 
+    //удалим элемент по индексу
     void removeByIndex(int index) {
-        //TODO
-        currentLength--;
+        if ((index >= currentLength) && (index < 0)) {
+            System.out.println("index = " + index + " is out of the list diapason");
+        } else {
+            int temp;
+            for (int i = index; i < currentLength - 1; i++) {
+                temp = array[i + 1];
+                array[i] = temp;
+            }
+            currentLength--;
+            System.out.println("removed element by index = " + index);
+        }
     }
 
+    //удалим элемент по значению
     void removeByValue(int value) {
-        //TODO
-        currentLength--;
+        int index = indexOf(value);
+        if (index == -1) {
+            System.out.println("There are no elements with value = " + value);
+        } else {
+            while (index != -1) {
+                removeByIndex(index);
+                index = indexOf(value);
+            }
+        }
     }
 
+    //индекс элемента по значению
     int indexOf(int value) {
         for (int i = 0; i < currentLength; i++) {
             if (array[i] == value) {
@@ -39,4 +61,15 @@ public class IntegerArrayList {
         }
         return -1;
     }
+
+    //отобразим array list
+    void print() {
+        String result = "[";
+        for (int i = 0; i < currentLength; i++) {
+            result = result + array[i] + ",";
+        }
+        result = result + "]";
+        System.out.println(result);
+    }
+
 }
--- Homeworks/Homework_11/src/lists/Main.java
+++ Homeworks/Homework_11/src/lists/Main.java
@@ -4,7 +4,53 @@ public class Main {
     public static void main(String[] args) {
         IntegerArrayList list = new IntegerArrayList();
         list.array = new int[10];
-        list.add(1000);
-        System.out.println(list.get(0));
+        list.print();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.print();
+        list.add(6);
+        list.add(7);
+        list.add(8);
+        list.add(9);
+        list.add(10);
+        list.add(11);
+        list.print();
+        list.removeByIndex(10);
+        list.print();
+        list.removeByIndex(6);
+        list.print();
+        list.add(8);
+        list.print();
+        list.removeByValue(8);
+        list.print();
     }
 }
+//возврат консоли:
+/*
+[]
+added element = 1 by index = 0
+added element = 2 by index = 1
+added element = 3 by index = 2
+added element = 4 by index = 3
+added element = 5 by index = 4
+[1,2,3,4,5,]
+added element = 6 by index = 5
+added element = 7 by index = 6
+added element = 8 by index = 7
+added element = 9 by index = 8
+added element = 10 by index = 9
+Can't add element 11. The list is full
+[1,2,3,4,5,6,7,8,9,10,]
+removed element by index = 10
+[1,2,3,4,5,6,7,8,9,]
+removed element by index = 6
+[1,2,3,4,5,6,8,9,]
+added element = 8 by index = 8
+[1,2,3,4,5,6,8,9,8,]
+removed element by index = 6
+removed element by index = 7
+[1,2,3,4,5,6,9,]
+*/
