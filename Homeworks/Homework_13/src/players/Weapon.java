1d830df1c9edf1dacb16da399b8d291c89285cb6
reiler1945 <49761127+reiler1945@users.noreply.github.com>
2019-05-23 14:33:28 +0300

    Update Weapon.java

--- Homeworks/Homework_13/src/players/Weapon.java
+++ Homeworks/Homework_13/src/players/Weapon.java
@@ -1,18 +1,33 @@
 package players;
 public abstract class Weapon {
+    
+    //повреждение для врага
     protected static int damage;
+    
+    //бонус в случае нанесения ущерба врагу
     protected static int bonus;
+    
+    //владелец оружия
+    protected PlayerUpgraded owner;
 
     // действие оружия
     public abstract int action();
 
-    // нанести урон
+    // нанесение урона
     public void doDamage(Player player) {
         this.action();
         player.setHealth(player.getHealth() - this.damage);
     }
-
+    
     public int getBonus() {
         return bonus;
+    }
+    
+    public int getOwner() {
+        return owner;
+    }
+
+    protected void setOwner(PlayerUpgraded owner) {
+        this.owner = owner;
     }
 }
