13ebad2c0b7a1fc1cab858f0911e6d317bdeae84
reiler <reiler@ya.ru>
2019-10-07 23:49:32 +0300

    add "extends SpringBootServletInitializer" to SpringBootApplication.class

--- Homeworks/InternetMagazineFinal/src/main/java/ru/itis/web/Application.java
+++ Homeworks/InternetMagazineFinal/src/main/java/ru/itis/web/Application.java
@@ -2,11 +2,12 @@ package ru.itis.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.ApplicationContext;
 
 //@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @SpringBootApplication
-public class Application{
+public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         ApplicationContext context = SpringApplication. run(Application.class, args);
